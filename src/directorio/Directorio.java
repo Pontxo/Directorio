8ca596a5f0ff150128ab95ff7fd7cb793c210917
Pontxo <angelic.rezzo@gmail.com>
2016-01-12 12:51:16 -0800

    - Removiendo permiso no utilizado.

--- src/directorio/Directorio.java
+++ src/directorio/Directorio.java
@@ -5,7 +5,6 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.sql.Connection;
